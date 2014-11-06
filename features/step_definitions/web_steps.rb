91e34363d5224db3442a53ca0288e0bb1aaa938d
eeasterberg <eeasterberg@yahoo.com>
2014-11-05 23:31:07 -0600

    Add validation to ensure names are specified when creating projects

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -11,3 +11,7 @@ Given(/^I am on the homepage$/) do
  Then(/^I should be shown the project created verification$/) do
    page.should have_content("Project has been created.")
  end
+ And(/^I should be on the project page for the new project$/) do
+#   current_path.should == project_path(Project.find_by_name!('TextMate 2'))
+   page.should have_content("TextMate 2 - Projects - Ticketee")
+ end
